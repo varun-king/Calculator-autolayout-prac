8af21e60e133aef6b6b7236a1ca53cde4ef7fcee
Varun Patel <varunpatel95.canada@gmail.com>
2020-12-30 15:41:30 -0500

    Initial Commit

---
+++ Calculator Layout Manual/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator Layout Manual
+//
+//  Created by apple on 12/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
